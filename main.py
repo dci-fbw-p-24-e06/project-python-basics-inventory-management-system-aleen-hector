79e29d95a6f2a908af76e19a61b675e06ac81885
Aleen_Dam <aleen.dam@dci-student.org>
2025-02-06 10:37:39 +0100

    half done

--- main.py
+++ main.py
@@ -8,10 +8,7 @@
 
 # create classes to represent products and inventory
 # class Product:
-'''def __init__(self, name, price, quantity):
-        self.name = name
-        self.price = price
-        self.quantity = quantity'''
+
 # create inventory manager to handle inventory operations
 
 # implement unit tests
@@ -32,62 +29,31 @@ InventoryManagementSystem/
 '''
 
 # main.py
-'''
+
 from inventory.inventory_manager import InventoryManager
 
 def main():
     inventory_manager = InventoryManager()
     inventory_manager.add_product('Product A', 10.99, 100)
     inventory_manager.add_product('Product B', 15.99, 50)
-
-    print(inventory_manager.calculate_total_inventory_value())  # Output: 2649.5
-
+    print(inventory_manager.get_total_inventory_value())  # Output: 549.5
     inventory_manager.update_quantity('Product A', 80)
-
-    print(inventory_manager.retrieve_product_information('Product A'))  # Output: Product A, 10.99, 80
-
+    print(inventory_manager.get_total_inventory_value())  # Output: 539.5
     inventory_manager.remove_product('Product B')
+    print(inventory_manager.get_total_inventory_value())  # Output: 539.5
+    inventory_manager.add_product('Product B', 15.99, 50)
 
-    print(inventory_manager.calculate_total_inventory_value())  # Output: 2549.5
+    print(inventory_manager.retrieve_product_information('Product A'))  # Output: Product A, 10.99, 100
+    print(inventory_manager.retrieve_product_information('Product B')) 
+    
+    
+
+
+
+
+
+    
 
 if __name__ == '__main__':
     main()
-'''
-
-# inventory/product.py
-'''
-class Product:
-    def __init__(self, name, price, quantity):
-        self.name = name
-        self.price = price
-        self.quantity = quantity
-'''
-
-# inventory/inventory_manager.py
-'''
-from inventory.product import Product
-
-class InventoryManager:
-    def __init__(self):
-        self.products = {}
-
-    def add_product(self, name, price, quantity):
-        if name in self.products:
-            raise ValueError(f'Product {name} already exists')
-        self.products[name] = Product(name, price, quantity)
-
-    def remove_product(self, name):
-        if name not in self.products:
-            raise ValueError(f'Product {name} does not exist')
-        del self.products[name]
-
-    def update_quantity(self, name, new_quantity):
-        if name not in self.products:
-            raise ValueError(f'Product {name} does not exist')
-        self.products[name].quantity = new_quantity
-
-    def retrieve_product_information(self, name):
-        if name not in self.products:
-            raise ValueError(f'Product {name}'''
-
 
